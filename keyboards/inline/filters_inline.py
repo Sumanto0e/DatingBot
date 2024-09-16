21ef798ab3c0c03ffa2dc665c8236c53963bed6c
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-16 16:17:10 +0700

    Update filters_inline.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/inline/filters_inline.py
+++ keyboards/inline/filters_inline.py
@@ -3,11 +3,6 @@ from aiogram.types import (
     InlineKeyboardMarkup,
 )
 
-from loader import (
-    _,
-)
-
-
 async def filters_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup()
     event_filters = InlineKeyboardButton(
