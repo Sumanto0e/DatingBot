50530b4c6d682774c1bcff63a7e6a67e2640589d
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-16 16:35:04 +0700

    Update payments.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/admin/inline/payments.py
+++ keyboards/admin/inline/payments.py
@@ -3,10 +3,6 @@ from aiogram.types import (
     InlineKeyboardMarkup,
 )
 
-from loader import (
-    _,
-)
-
 
 async def payments_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=1)
