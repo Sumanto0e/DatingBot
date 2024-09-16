74dc347c16d0fb3f35cc5da74fc389b42806466f
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-16 21:58:16 +0700

    Update get_photo.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/default/get_photo.py
+++ keyboards/default/get_photo.py
@@ -6,6 +6,6 @@ from aiogram.types import (
 
 async def get_photo_from_profile() -> ReplyKeyboardMarkup:
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    send_photo = KeyboardButton(text=_("Взять из профиля"))
+    send_photo = KeyboardButton(text=("Ambil dari profil"))
     markup.add(send_photo)
     return markup
