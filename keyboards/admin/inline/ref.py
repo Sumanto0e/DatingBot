dd88b02df8c02fb42e58abb8dc56531d4918d6dd
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-16 21:51:35 +0700

    Update ref.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/admin/inline/ref.py
+++ keyboards/admin/inline/ref.py
@@ -5,9 +5,9 @@ from aiogram.types import (
 
 async def referral_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=1)
-    statistics = InlineKeyboardButton(_("📈 Статистика"), callback_data="ref_urls:stats")
-    add_ref = InlineKeyboardButton(_("*️⃣ Добавить"), callback_data="ref_urls:create")
-    delete_ref = InlineKeyboardButton(_("❌ Удалить"), callback_data="ref_urls:delete")
-    back = InlineKeyboardButton(_("◀️ Назад"), callback_data="admin:mailing_md")
+    statistics = InlineKeyboardButton(("📈 Statistik"), callback_data="ref_urls:stats")
+    add_ref = InlineKeyboardButton(("*️⃣ Menambahkan"), callback_data="ref_urls:create")
+    delete_ref = InlineKeyboardButton(("❌ Menghapus"), callback_data="ref_urls:delete")
+    back = InlineKeyboardButton(("◀️ Kembali"), callback_data="admin:mailing_md")
     markup.add(statistics, add_ref, delete_ref, back)
     return markup
