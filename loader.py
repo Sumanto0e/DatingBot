4712bc6928674e90fa58fde6a4a3ed285f4fdcb7
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-17 03:02:06 +0700

    Update loader.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- loader.py
+++ loader.py
@@ -28,9 +28,6 @@ from utils.YandexMap.api import (
     Client,
 )
 
-from utils.yoomoney import (
-    YooMoneyWallet,
-)
 
 bot = Bot(token=load_config().tg_bot.token, parse_mode=types.ParseMode.HTML)
 storage = RedisStorage2() if load_config().tg_bot.use_redis else MemoryStorage()
@@ -40,7 +37,6 @@ job_defaults = dict(coalesce=False, max_instances=3)
 scheduler = AsyncIOScheduler(
     timezone=load_config().tg_bot.timezone, job_defaults=job_defaults
 )
-wallet = YooMoneyWallet(access_token=load_config().misc.yoomoney_key)
 detector = NudeDetector()
 
 logger = logging.getLogger(__name__)
