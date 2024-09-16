7af8c796ba050802f8ba2f91a5bbf37c170d471b
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-16 21:49:39 +0700

    Update mailing.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/admin/inline/mailing.py
+++ keyboards/admin/inline/mailing.py
@@ -6,7 +6,7 @@ from aiogram.types import (
 
 async def mailing_menu():
     markup = InlineKeyboardMarkup(row_width=1)
-    mailing = InlineKeyboardButton(_("📧 Рассылка"), callback_data="adv:mailing")
+    mailing = InlineKeyboardButton(("📧 Broadcast"), callback_data="adv:mailing")
     markup.add(mailing)
     return markup
 
@@ -14,12 +14,12 @@ async def mailing_menu():
 async def add_buttons_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=1)
     confirm_sending = InlineKeyboardButton(
-        text=_("Подтвердить отправку"), callback_data="confirm_send"
+        text=_("Konfirmasikan pengiriman"), callback_data="confirm_send"
     )
     add_button = InlineKeyboardButton(
-        text=_("Добавить кнопку"), callback_data="add_buttons"
+        text=("Tambahkan tombol"), callback_data="add_buttons"
     )
-    cancel = InlineKeyboardButton(text=_("Отмена"), callback_data="cancel")
+    cancel = InlineKeyboardButton(text=("Membatalkan"), callback_data="cancel")
 
     markup.row(confirm_sending, add_button)
     markup.add(cancel)
@@ -29,9 +29,9 @@ async def add_buttons_keyboard() -> InlineKeyboardMarkup:
 async def confirm_with_button_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=1)
     confirm_sending = InlineKeyboardButton(
-        text=_("Подтвердить отправку"), callback_data="confirm_send_with_button"
+        text=("Konfirmasikan pengiriman"), callback_data="confirm_send_with_button"
     )
-    cancel = InlineKeyboardButton(text=_("Отмена"), callback_data="cancel")
+    cancel = InlineKeyboardButton(text=("Membatalkan"), callback_data="cancel")
     markup.add(confirm_sending)
     markup.add(cancel)
     return markup
