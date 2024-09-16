7f6b4da831eda2c25d72b8b9d22e2d7631cb8e9b
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-17 04:02:40 +0700

    Update registration_inline.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/inline/registration_inline.py
+++ keyboards/inline/registration_inline.py
@@ -19,7 +19,7 @@ async def second_registration_keyboard() -> InlineKeyboardMarkup:
 async def confirm_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=2)
     yes_all_good = InlineKeyboardButton(
-        text= ("✅ Ya semuanya baik-baik saja!"), callback_data="yes_all_good"
+        text= ("✅ Ya itu kota saya!"), callback_data="yes_all_good"
     )
     markup.add(yes_all_good)
     return markup
