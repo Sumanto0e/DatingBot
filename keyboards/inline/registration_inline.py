978c32215918d867820847eb69d6bdd2d0a9d702
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-16 16:59:08 +0700

    Update registration_inline.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/inline/registration_inline.py
+++ keyboards/inline/registration_inline.py
@@ -7,10 +7,10 @@ from aiogram.types import (
 async def second_registration_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=1)
     questionnaire = InlineKeyboardButton(
-        text=_("🖌️ Пройти опрос в боте"), callback_data="survey"
+        text= ("🖌️ Пройти опрос в боте"), callback_data="survey"
     )
     back_to_menu = InlineKeyboardButton(
-        text=_("⏪️ Вернуться в меню"), callback_data="start_menu"
+        text= ("⏪️ Вернуться в меню"), callback_data="start_menu"
     )
     markup.add(questionnaire, back_to_menu)
     return markup
@@ -19,7 +19,7 @@ async def second_registration_keyboard() -> InlineKeyboardMarkup:
 async def confirm_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=2)
     yes_all_good = InlineKeyboardButton(
-        text=_("✅ Да все хорошо!"), callback_data="yes_all_good"
+        text= ("✅ Да все хорошо!"), callback_data="yes_all_good"
     )
     markup.add(yes_all_good)
     return markup
