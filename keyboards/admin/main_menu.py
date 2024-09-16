454a9f43417276cd5654061f79570d9a547adfcf
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-16 21:54:38 +0700

    Update main_menu.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/admin/main_menu.py
+++ keyboards/admin/main_menu.py
@@ -6,12 +6,12 @@ from aiogram.types import (
 
 async def admin_keyboard() -> ReplyKeyboardMarkup:
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    customers = KeyboardButton("🫂 Пользователи")
-    settings = KeyboardButton("⚙️ Настройки")
-    advert = KeyboardButton("📊 Реклама")
-    logs = KeyboardButton("🗒 Логи")
-    monitoring = KeyboardButton(text="👀 Мониторинг")
-    set_up_technical_works = KeyboardButton(text="🛑 Тех.Работа")
+    customers = KeyboardButton("🫂 Pengguna")
+    settings = KeyboardButton("⚙️ Pengaturan")
+    advert = KeyboardButton("📊 Periklanan")
+    logs = KeyboardButton("🗒 Log")
+    monitoring = KeyboardButton(text="👀 Pemantauan")
+    set_up_technical_works = KeyboardButton(text="🛑 Pekerjaan teknis")
     markup.add(customers, monitoring)
     markup.add(settings)
     markup.add(logs, advert)
