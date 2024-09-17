1feb5dc473fe5588617c04963e0c3559ce07aefe
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-18 00:26:30 +0700

    Update menu_profile_inline.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/inline/menu_profile_inline.py
+++ keyboards/inline/menu_profile_inline.py
@@ -37,3 +37,30 @@ async def dating_filters_keyboard() -> InlineKeyboardMarkup:
     markup.row(user_need_gender, user_age_period)
     markup.add(back)
     return markup
+    
+async def change_info_keyboard() -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(row_width=2)
+    name = InlineKeyboardButton(text=("👤 Nama"), callback_data="name")
+    gender = InlineKeyboardButton(text=("⚧ Gender"), callback_data="gender")
+    age = InlineKeyboardButton(text=("📅 Usia"), callback_data="age")
+    city = InlineKeyboardButton(text=("🏙 Kota"), callback_data="city")
+    photo = InlineKeyboardButton(text=("📷 Photo profile"), callback_data="photo")
+    about_me = InlineKeyboardButton(text=("📝 status"), callback_data="about_me")
+    back_to_menu = InlineKeyboardButton(
+        text=("⏪️ Kembali ke menu"), callback_data="back_to_profile_menu"
+    )
+    markup.row(name, gender, age)
+    markup.row(city, photo, about_me)
+    markup.add(back_to_menu)
+    return markup
+
+async def gender_keyboard(m_gender: str, f_gender: str) -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(row_width=1)
+    male = InlineKeyboardButton(text=m_gender, callback_data="male")
+    female = InlineKeyboardButton(text=f_gender, callback_data="female")
+    stopped = InlineKeyboardButton(
+        text=("❌ Berhenti"), callback_data="registration:stopped"
+    )
+    markup.row(male, female)
+    markup.add(stopped)
+    return markup
