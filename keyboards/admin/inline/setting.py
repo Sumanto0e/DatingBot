f481bb12e2d1f75eab46c130d423b5524af14295
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-17 15:25:37 +0700

    Update setting.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/admin/inline/setting.py
+++ keyboards/admin/inline/setting.py
@@ -6,9 +6,9 @@ from aiogram.types import (
 
 async def add_admins_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup()
-    add = InlineKeyboardButton(_("*️⃣ Menambahkan"), callback_data="admin:admins:add")
-    delete = InlineKeyboardButton(_("❌ Menghapus"), callback_data="admin:admins:delete")
-    back = InlineKeyboardButton(_("◀️ Kembali"), callback_data="admin:settings")
+    add = InlineKeyboardButton(("*️⃣ Menambahkan"), callback_data="admin:admins:add")
+    delete = InlineKeyboardButton(("❌ Menghapus"), callback_data="admin:admins:delete")
+    back = InlineKeyboardButton(("◀️ Kembali"), callback_data="admin:settings")
     markup.add(add, delete)
     markup.add(back)
     return markup
