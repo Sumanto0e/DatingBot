e10330d12593525a5036455c3c266226d27a90ef
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-18 00:58:17 +0700

    Update registration.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- handlers/users/registration.py
+++ handlers/users/registration.py
@@ -137,7 +137,6 @@ async def get_name(message: types.Message, state: FSMContext) -> None:
 # TODO: Убрать возможность у пользователя использовать ввод для определения города
 @dp.message_handler(state=RegData.age)
 async def get_age(message: types.Message, state: FSMContext) -> None:
-    markup = await location_keyboard()
     await state.update_data(age=message.text)
     try:
         if 10 < int(message.text) < 99:
@@ -157,7 +156,6 @@ async def get_age(message: types.Message, state: FSMContext) -> None:
         return
     await message.answer(
         text=("Di kota mana anda tinggal?"),
-        reply_markup=markup,
     )
     await RegData.town.set()
 
