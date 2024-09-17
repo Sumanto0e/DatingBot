d3f25fdb8ece261c244fb95974b87558eb2721b5
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-17 23:23:46 +0700

    Update menu_profile_inline.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/inline/menu_profile_inline.py
+++ keyboards/inline/menu_profile_inline.py
@@ -11,10 +11,30 @@ async def get_profile_keyboard(verification) -> InlineKeyboardMarkup:
         )
         markup.row(verification_btn)
     edit_profile = InlineKeyboardButton(
-        text=("🖊 Setting akun"), callback_data="change_profile"
+        text=("🖊 Pengaturan akun"), callback_data="change_profile"
+    )
+        dating_filters = InlineKeyboardButton(
+        text=("❤️ Pengaturan kenalan"), callback_data="dating_filters"
     )
     turn_off = InlineKeyboardButton(text=("🗑️ Menghapus"), callback_data="disable")
     back = InlineKeyboardButton(text=("⏪ Kembali"), callback_data="back_with_delete")
     markup.row(edit_profile, turn_off)
+    markup.add(back)
+    return markup
+
+async def dating_filters_keyboard() -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup()
+    user_need_city = InlineKeyboardButton(
+        text=("🏙️ kota pasangan anda"), callback_data="needs_city"
+    )
+    user_age_period = InlineKeyboardButton(
+        text=("🔞Rentang usia"), callback_data="user_age_period"
+    )
+    user_need_gender = InlineKeyboardButton(
+        text=("🚻 jenis kelamin pasangan"), callback_data="user_need_gender"
+    )
+    back = InlineKeyboardButton(text=("⏪️ Kembali"), callback_data="back_to_filter_menu")
+    markup.add(user_need_city)
+    markup.row(user_need_gender, user_age_period)
     markup.add(back)
     return markup
