4cb0bb3ec5f5dcd83d94050d1e458cd0eee5cce8
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-19 17:44:44 +0700

    Update registration.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- handlers/users/registration.py
+++ handlers/users/registration.py
@@ -324,3 +324,47 @@ async def finish_filter(message: types.Message, state: FSMContext) -> None:
     await message.answer_photo(
         caption=user_info, photo=user.photo_id, reply_markup=markup
     )
+
+@dp.callback_query_handler(text="add_inst")
+async def add_inst(call: CallbackQuery, state: FSMContext) -> None:
+    await delete_message(call.message)
+    await call.message.answer(
+        text=(
+            "Tulis nama akun Anda\n\n"
+            "Contoh:\n"
+            "<code>@fwabase</code>\n"
+            "<code>https://www.instagram.com/fwabase</code>"
+        )
+    )
+    await state.set_state("inst")
+
+
+@dp.message_handler(state="inst")
+async def add_inst_state(message: types.Message, state: FSMContext) -> None:
+    try:
+        markup = await start_keyboard(obj=message)
+        inst_regex = r"([A-Za-z0-9._](?:(?:[A-Za-z0-9._]|(?:\.(?!\.))){2,28}(?:[A-Za-z0-9._]))?)$"
+        regex = re.search(inst_regex, message.text)
+        result = regex
+        if bool(regex):
+            await state.update_data(inst=message.text)
+            await db_commands.update_user_data(
+                instagram=result[0], telegram_id=message.from_user.id
+            )
+            await message.answer(text=("Akun Anda telah berhasil ditambahkan"))
+            await asyncio.sleep(1)
+            await state.reset_state()
+            await message.answer(
+                text=("Вы были возвращены в меню"), reply_markup=markup
+            )
+        else:
+            await message.answer(
+                text=(
+                    "Anda memasukkan tautan atau nama akun yang salah.\n\nContoh:\n"
+                    "<code>@unknown</code>\n<code>https://www.instagram.com/unknown</code>"
+                )
+            )
+
+    except DataError:
+        await message.answer(text=("Telah terjadi kesalahan. coba lagi"))
+        return
