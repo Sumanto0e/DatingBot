c98456be9da2ddcf0e39e2775e6f62f4502fa563
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-23 05:22:20 +0700

    Update caripasangan.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/default/caripasangan.py
+++ keyboards/default/caripasangan.py
@@ -1,1 +1,13 @@
+rom aiogram.types import (
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+)
 
+
+async def location_keyboard() -> ReplyKeyboardMarkup:
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    send_location = KeyboardButton(
+        text=("🗺 Kirim lokasi secara otomatis"), request_location=True
+    )
+    markup.add(send_location)
+    return markup
