09651c3bcf190ad510eabc4e96c78db121792fbc
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-24 02:51:53 +0700

    Update reaction_strategies.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- functions/dating/reaction_strategies.py
+++ functions/dating/reaction_strategies.py
@@ -324,7 +324,7 @@ class ChooseReportReason(ActionStrategy):
             message_id=call.message.message_id,
             reply_markup=None,
         )
-        target_id = user_for_like)
+        target_id = user_for_like
         await call.message.answer(
             text=("<u>Pilih alasan keluhan:</u>"),
             reply_markup=await report_menu_keyboard(telegram_id=target_id),
