7f7a0f3e5d444611608d4332946dc498af52cf29
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-09-26 19:30:02 +0700

    Update payments_inline.py
    Signed-off-by: Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>

--- keyboards/inline/payments_inline.py
+++ keyboards/inline/payments_inline.py
@@ -9,16 +9,13 @@ from yarl import (
 
 async def payment_menu_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup()
-    yoomoney = InlineKeyboardButton(text=_("💳 hubungi"), url="https://t.me/nazhak")
+    yoomoney = InlineKeyboardButton(text=("💳 hubungi"), url="https://t.me/nazhak")
     markup.add(yoomoney)
     return markup
 
 
 async def yoomoney_keyboard(url: str | URL = None) -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=1)
-    pay_yoomoney = InlineKeyboardButton(text=_("💳 Membayar"), url=url)
-    check_prices = InlineKeyboardButton(
-        text=_("🔄 Проверить оплату"), callback_data="yoomoney:check_payment"
-    )
-    markup.add(pay_yoomoney, check_prices)
+    pay_yoomoney = InlineKeyboardButton(text=("💳 Membayar"), url="https://t.me/nazhak")
+    markup.add(pay_yoomoney)
     return markup
